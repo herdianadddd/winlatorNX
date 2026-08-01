4e83be18a04d917960a4893dcaf09f5d006bfbcb
herdianadddd <101114400+herdianadddd@users.noreply.github.com>
2026-08-01 18:33:30 +0700

    runtime.cpp

---
+++ source/runtime.cpp
@@ -0,0 +1,27 @@
+#include "runtime.hpp"
+
+#include <stdio.h>
+
+
+bool Runtime::launch(
+const char* exe
+)
+{
+
+printf(
+"Launching %s\n",
+exe
+);
+
+
+/*
+future:
+
+box64 wine.exe
+
+*/
+
+
+return true;
+
+}
